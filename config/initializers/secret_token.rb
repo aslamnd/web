a54aa1f7002eaa27d85c8583c0fe6cbb27fa2f8c
Josep Jaume <josepjaume@gmail.com>
2011-01-28 17:17:26 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodegramWeb::Application.config.secret_token = 'd0fb81454494836b1de281ad7a9535ff7c5408b706a7a80333c103c6f44a70e97e28e73711f6351a7e642e3bda3e3d2e0e076101d66569ecbcd4bdd270ca8e09'
---
+++ spec/factories/user.rb
@@ -0,0 +1,5 @@
+Factory.define :user do |u|
+  u.email 'admin@codegram.com'
+  u.password 'admin123'
+  u.password_confirmation 'admin123'
+end
