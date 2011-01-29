3242c211ae147e3d41d060f8b528e94df6d8036c
Josep Jaume <josepjaume@gmail.com>
2011-01-29 02:54:47 +0100

    Work in the layout

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,15 @@
-// $(document).ready(function(){
-//   $("#header h1").lettering();
-// });
+$(document).ready(function(){
+
+  var content_height = $("#slideshow").height() + $("#content").height();
+  var window_height = $(window).height();
+  var total_height = 0;
+  if(content_height > window_height){
+    total_height = content_height;
+  }else{
+    total_height = window_height;
+  }
+
+  if($("#sidebar-inner").height() < total_height){
+    $("#sidebar-inner").css('height',total_height);
+  }
+});
