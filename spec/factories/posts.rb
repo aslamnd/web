03ce04d4cfdd827f1772c4926f323d18a110f551
Josep M. Bach <josep.m.bach@gmail.com>
2011-02-02 11:11:43 +0100

    Users can edit post dates

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,5 @@
 Factory.define :post do |p|
   p.title { Faker::Company.catch_phrase }
-  p.body { Faker::Lorem.paragraphs }
+  p.body { Faker::Lorem.paragraphs.join("\n") }
   p.association :author
 end
