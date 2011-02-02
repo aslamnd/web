25b9fdca04a3c6c735265a6138f6796e2de8c099
Josep M. Bach <josep.m.bach@gmail.com>
2011-02-02 10:42:47 +0100

    Date format helper for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,7 @@ class Post < ActiveRecord::Base
   delegate :year, to: :created_at
   delegate :month, to: :created_at
 
+  delegate :name, to: :author, prefix: true
+
   has_friendly_id :title, use_slug: true
 end
