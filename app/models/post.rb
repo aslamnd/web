ea704456e7a7691a9897a5b91d27dbc4656928d8
Josep M. Bach <josep.m.bach@gmail.com>
2011-02-03 11:56:21 +0100

    Blog post seeds

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   markdownize! :body
 
   belongs_to :author, :class_name => 'User'
+  validates_associated :author
 
   delegate :year, to: :created_at
   delegate :month, to: :created_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,25 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 #
-Factory.create(:user)
+# Blog seeds
+
+User.destroy_all
+user = Factory(:user, name: 'Josep M. Bach', email: 'txus@codegram.com')
+
+stendhal_post = Factory(:post,
+                        title: 'Stendhal 0.1.2 released',
+                        body: File.read('db/seeds/stendhal.md'),
+                        created_at: Date.parse('2010-10-31'),
+                        author: user)
+
+hijacker_post = Factory(:post,
+                         title: 'Introducing hijacker: spy on your ruby objects!',
+                         body: File.read('db/seeds/hijacker.md'),
+                         created_at: Date.parse('2010-11-21'),
+                         author: user)
+
+micetrap_post = Factory(:post,
+                        title: 'Micetrap: catch evil hackers on the fly',
+                        body: File.read('db/seeds/micetrap.md'),
+                        created_at: Date.parse('2011-01-06'),
+                        author: user)
