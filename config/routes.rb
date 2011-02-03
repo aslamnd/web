b81e4575d5790143dac4bd9065480a25d7eaf07a
Josep M. Bach <josep.m.bach@gmail.com>
2011-02-03 15:55:44 +0100

    Awesome atom for blog

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,12 @@ CodegramWeb::Application.routes.draw do
 
   # Public sections
   constraints subdomain: 'blog' do
-    match '/' => 'posts#index', as: :blog, via: :get
-    match '(/:year)(/:month)' => 'posts#index', as: :posts, via: :get
-    resources :posts, { path: '/:year/:month', only: :show }
+    constraints format: :html do
+      match '/' => 'posts#index', as: :blog, via: :get
+      match '(/:year)(/:month)' => 'posts#index', as: :posts, via: :get
+      resources :posts, { path: '/:year/:month', only: :show }
+    end
+    match '/feed.atom' => 'posts#index', as: :feed, via: :get, format: :atom
   end
 
   constraints BaseSubdomain do
@@ -19,11 +22,13 @@ CodegramWeb::Application.routes.draw do
     resource :contact, :controller => 'contact_forms',
       :only => [:new, :create],
       :path_names => {:new => '/'}
+    match '/feed.atom' => redirect('http://blog.codegram.com/feed.atom', status: 302)
     match '/:id', :controller => 'high_voltage/pages', :action => :show
   end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
+
   root :to => "high_voltage/pages#show", :id => 'home'
 
   # See how all your routes lay out with "rake routes"
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -16,4 +16,42 @@ describe Post do
     it { should delegate(:year).to(:created_at) }
   end
 
+  describe "validations" do
+    it { should validate_presence_of(:author) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
+
+  describe "scopes" do
+    describe ".from_archive" do
+
+      let(:february_post) do
+        Factory.create :post, created_at: Date.new(2010,2,10)
+      end
+      let(:future_post) do
+        Factory.create :post, created_at: Date.new(2011,2,10)
+      end
+
+      before do
+        Factory.create :post, created_at: Date.new(2010,3,10)
+      end
+
+      context "provided only a year" do
+        let(:args) { ['2011']}
+        it 'returns posts of that year' do
+          expected = future_post
+          Post.from_archive(*args).all.should == [expected]
+        end
+      end
+      context "provided a year and a month" do
+        let(:args) { ['2010', '02']}
+        it 'returns posts of that month' do
+          expected = february_post
+          Post.from_archive(*args).all.should == [expected]
+        end
+      end
+
+    end
+  end
+
 end
