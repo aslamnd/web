ba0764432583020b0d58d4beeaeb1d8c8990e3bb
Josep Jaume <josepjaume@gmail.com>
2011-02-04 18:08:19 +0100

    Update projects

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,27 @@
 
 user = User.find_by_email('txus@codegram.com') || Factory(:user, name: 'Josep M. Bach', email: 'txus@codegram.com')
 
-stendhal_post = Factory(:post,
-                        title: 'Stendhal 0.1.2 released',
-                        body: File.read('db/seeds/stendhal.md'),
-                        created_at: Date.parse('2010-10-31'),
-                        author: user)
+unless Rails.env.production?
 
-hijacker_post = Factory(:post,
-                         title: 'Introducing hijacker: spy on your ruby objects!',
-                         body: File.read('db/seeds/hijacker.md'),
-                         created_at: Date.parse('2010-11-21'),
-                         author: user)
+  stendhal_post = Factory(:post,
+                          title: 'Stendhal 0.1.2 released',
+                          body: File.read('db/seeds/stendhal.md'),
+                          created_at: Date.parse('2010-10-31'),
+                          author: user)
 
-micetrap_post = Factory(:post,
-                        title: 'Micetrap: catch evil hackers on the fly',
-                        body: File.read('db/seeds/micetrap.md'),
-                        created_at: Date.parse('2011-01-06'),
-                        author: user)
+  hijacker_post = Factory(:post,
+                           title: 'Introducing hijacker: spy on your ruby objects!',
+                           body: File.read('db/seeds/hijacker.md'),
+                           created_at: Date.parse('2010-11-21'),
+                           author: user)
+
+  micetrap_post = Factory(:post,
+                          title: 'Micetrap: catch evil hackers on the fly',
+                          body: File.read('db/seeds/micetrap.md'),
+                          created_at: Date.parse('2011-01-06'),
+                          author: user)
+
+  20.times do
+    Factory(:project)
+  end
+end
