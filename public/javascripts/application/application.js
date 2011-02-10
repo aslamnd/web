9f4014133b10fc76e2e2082ad6c500ae1855faef
Josep Jaume <josepjaume@gmail.com>
2011-02-10 09:25:47 +0100

    Bugfix work in explorer 8

--- public/javascripts/application/application.js
+++ public/javascripts/application/application.js
@@ -1,5 +1,11 @@
 $.easing.bounce = function (x, t, b, c, d) {
-	var s = 0;
+	var s = 0.1;
+	if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+	return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+}
+
+$.easing.heavy_bounce = function (x, t, b, c, d) {
+	var s = 0.8;
 	if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
 	return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
 }
@@ -7,9 +13,3 @@ $.easing.bounce = function (x, t, b, c, d) {
 $(function(){
   $('input, textarea').placeholder();
 });
-
-$.easing.heavy_bounce = function (x, t, b, c, d) {
-	var s = 0.8;
-	if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
-	return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
-}
