220e3ee1c29bdf6fa324d07f2ef5f448a7377f95
Oriol Gual <oriol.gual@gmail.com>
2011-02-14 11:07:40 +0100

    Fix post factory

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,4 +3,5 @@ Factory.define :post do |p|
   p.tagline { "#{Faker::Company.catch_phrase}, built on top of #{Faker::Company.catch_phrase.tap {|n| n[0] = n[0].downcase }}" }
   p.body { Faker::Lorem.paragraphs.join("\n") }
   p.association :author
+  p.published { true }
 end
