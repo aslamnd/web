8773a3e65654b091ba2f50af222392a21350b431
Oriol Gual <oriol.gual@gmail.com>
2011-02-14 12:05:03 +0100

    404 when not a valid post URL. Fixes #20

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -5,3 +5,11 @@ Then /^I should see (\d+) posts$/ do |num|
     page.should_not have_css('.post')
   end
 end
+
+When /^I visit a fake post$/ do
+  visit '/tags/fake'
+end
+
+Then /^I should get a (\d+) error$/ do |arg1|
+  page.status_code == 404
+end
