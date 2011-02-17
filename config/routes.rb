ac54fbaac9427e5f2b4daccaadd6cc3af761ce0e
Oriol Gual <oriol.gual@gmail.com>
2011-02-17 14:52:14 +0100

    Redirect old date validator post

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ CodegramWeb::Application.routes.draw do
   
   ## Blog subdomain
   constraints subdomain: 'blog' do
+    match '/en/2010/04/30/presenting-date-validator.html' => redirect('/2011/2/date-validation-with-rails-3', status: 301)
+    match '/es/2010/04/29/presenting-date-validator.html' => redirect('/2011/2/date-validation-with-rails-3', status: 301)
     namespace :blog, path: '/' do
       match '/sitemap.xml' => 'sitemaps#show'
       constraints format: :html do
