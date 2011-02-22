fa362e504e6a8e7ca4f319a8ce82e5c09c828ea3
Josep Jaume <josepjaume@gmail.com>
2011-02-22 16:55:38 +0100

    Make migration publish all projects

--- db/migrate/20110221152827_add_published_flag_to_projects.rb
+++ db/migrate/20110221152827_add_published_flag_to_projects.rb
@@ -1,6 +1,9 @@
 class AddPublishedFlagToProjects < ActiveRecord::Migration
   def self.up
     add_column :projects, :published, :boolean
+    Project.all.each do |project|
+      project.update_attribute(:published, true)
+    end
   end
 
   def self.down
