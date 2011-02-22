8bd72bad4f23c0ab21e0ffa70d8db0e07876a817
Josep Jaume <josepjaume@gmail.com>
2011-02-22 17:01:24 +0100

    Update migration

--- db/migrate/20110221152827_add_published_flag_to_projects.rb
+++ db/migrate/20110221152827_add_published_flag_to_projects.rb
@@ -1,6 +1,7 @@
 class AddPublishedFlagToProjects < ActiveRecord::Migration
   def self.up
     add_column :projects, :published, :boolean
+
     Project.all.each do |project|
       project.update_attribute(:published, true)
     end
