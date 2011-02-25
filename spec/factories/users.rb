fb229a4eaa14a2c5a18fdd729c039b6c633d4aee
mrc2407 <mrc2407@gmail.com>
2011-02-25 20:59:32 +0100

    Update user factories and blog user page styles

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ Factory.define :user do |u|
   u.password 'admin123'
   u.password_confirmation 'admin123'
   u.user_tokens {|n| n.user_tokens << Factory(:user_token) }
+  u.description 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.'
 end
 
 Factory.define :author, parent: :user do |a|
