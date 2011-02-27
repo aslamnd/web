09e2c2f0227414590256bf77cf9611e25e6a2e0d
mrc2407 <mrc2407@gmail.com>
2011-02-27 23:41:38 +0100

    Add homepage field to blog user page. Fixes #29

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110225105642) do
+ActiveRecord::Schema.define(:version => 20110227220711) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(:version => 20110225105642) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
+    t.string   "homepage"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
