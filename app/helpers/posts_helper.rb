5e6521fc322175b1e0517058be71a6a5b4a93491
Josep Jaume <josepjaume@gmail.com>
2011-04-08 09:42:15 +0200

    Add comment count to the post index page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,6 +4,11 @@ module PostsHelper
     link_to(post.title, post_url(post, options))
   end
 
+  def comment_count(post, options={})
+    options[:host] ||= false
+    link_to("Comments", post_url(post, options) + "#disqus_thread")
+  end
+
   def post_url(post, options={})
     options[:host] ||= false
     (options[:host] ? "http://blog.codegram.com" : '') + blog_post_path(post, year: post.year, month: post.month)
