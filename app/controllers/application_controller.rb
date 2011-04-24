e34a3c9e2586ff194eb00c7901e8253265a92a7c
Josep Jaume <josepjaume@gmail.com>
2011-04-24 16:59:52 +0200

    Remove cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,32 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_locale
+
   unless Rails.env.development?
     rescue_from ActiveRecord::RecordNotFound, with: :not_found
     rescue_from ActiveRecord::RecordInvalid, with: :not_found
   end
 
 private
+
+  def set_locale
+    locale =
+      if !params[:locale].blank?
+        params[:locale]
+      elsif session && !session[:locale].blank?
+        session[:locale]
+      elsif request.headers['HTTP_ACCEPT_LANGUAGE']
+        request.headers['HTTP_ACCEPT_LANGUAGE'].split(',')[0].split('-')[0]
+      end
+
+    if ['ca', 'es', 'en'].include? locale
+      I18n.locale = locale
+    else
+      I18n.locale = 'en'
+    end
+    session[:locale]=I18n.locale.to_s
+  end
 
   def not_found(error)
     @error = get_data_for_error 
@@ -21,12 +41,6 @@ private
 
   def after_sign_in_path_for(resource_or_scope)
     admin_path
-  end
-
-  def set_cache_control
-    if Rails.env.production?
-      expires_in 5.minutes, public: true
-    end
   end
 
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,2 @@
 class PagesController < HighVoltage::PagesController
-  before_filter :set_cache_control
-
-private
-
-  def set_cache_control
-    if Rails.env.production?
-      expires_in 24.hours, :public => true unless user_signed_in?
-    end
-  end
-
 end
