3cb3b94db56bef53201fb5d6d8cdeba413afd500
Marc Riera Casals <mrc2407@gmail.com>
2011-05-30 12:29:00 +0200

    Define all new steps

--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -1,3 +1,7 @@
+Given /^a project called "([^"]*)" exists$/ do |name|
+  create_model(:project, title: name)
+end
+
 Then /^I should see (\d+) projects?$/ do |num|
   if num.to_i > 0
     page.should have_css('.project', count: num.to_i)
